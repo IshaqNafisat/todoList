e54b257942d7c326f02bcc885c4e2256e2329be5
Hazeezat02 <ishaqnafisat18@gmmail.com>
2026-08-16 01:52:25 +0100

    update deploy

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css"
+import TodoList from "./todo";
 
 const App = () => {
   return (
     <div>
       <h1></h1>
+      <TodoList/>
     </div>
   )
 }
